ed885c54965d0dc6e6bb85d3673f47655e94ec3a
Jim Kang <jimkang@gmail.com>
2015-03-12 22:52:17 -0400

    Added guys, restructured loop to make the guys make plans and tick the clock and rerender.

--- loop.js
+++ loop.js
@@ -5,52 +5,159 @@ var createProbable = require('probable').createProbable;
 var seedrandom = require('seedrandom');
 var createGriddler = require('./griddler').create;
 var _ = require('lodash');
+var createClock = require('./clock').create;
+var createMove = require('./actions/move').create;
+var callBackOnNextTick = require('conform-async').callBackOnNextTick;
+var queue = require('queue-async');
+
+var seed = (new Date).getTime().toString();
+
+console.log('Seed:', seed);
+
+var probable = createProbable({
+  random: seedrandom(seed)
+});
+
+var floorRenderer;
+var floorCells;
+var guyRenderer;
+var guyCells;
+var griddler;
+var clock;
 
 function start() {
-  var seed = (new Date).getTime().toString();
+  griddler = createGriddler();
 
-  console.log('Seed:', seed);
-
-  var probable = createProbable({
-    random: seedrandom(seed)
+  clock = createClock({
+    flipCoin: function flipCoin() {
+      return probable.roll(2) === 0;
+    }
   });
 
-  var griddler = createGriddler();
+  floorCells = d3.range(100).map(generateFloorCell);
+  var setFloorCell = _.curry(griddler.setCell)('floor');
+  floorCells.forEach(setFloorCell);
 
-  var floorRenderer = createLayerRenderer({
+  floorRenderer = createLayerRenderer({
     cellWidth: 25,
     cellHeight: 25,
     cellClass: 'floor-cell',
     layerSelector: '.floor',
   });
 
-  function generateCell() {
-    return {
-      id: idmaker.randomId(5),
-      live: {
-        coords: [
-          probable.roll(10),
-          probable.roll(10)
-        ]
-      },
-      next: {
-        coords: []
-      }
-    };
-  }
 
-  var floorCells = d3.range(100).map(generateCell);
-  var setFloorCell = _.curry(griddler.setCell)('floor');
-  floorCells.forEach(setFloorCell);
-
-  floorRenderer.render(griddler.getLayer('floor'));
-
-  var floor = d3.select('.streets');
+  var floor = d3.select('.floor');
   floor.on('click', function floorClicked() {
     var point = d3.mouse(floor.node());
     var coords = floorRenderer.pointToCoords(point);
     console.log('Click at ', point, 'which maps to coords', coords);
+
+    kickOffPlanning();
   });
+
+  guyCells = d3.range(5).map(generateGuyCell);
+
+  var setGuyCell = _.curry(griddler.setCell)('guys');
+  guyCells.forEach(setGuyCell);
+
+  guyRenderer = createLayerRenderer({
+    cellWidth: 25,
+    cellHeight: 25,
+    cellClass: 'guy-cell',
+    layerSelector: '.guys'
+  });
+
+  clock.on('tickDone', advanceAllToNextState);
+
+  renderLayers();
+}
+
+function kickOffPlanning() {
+  var planPack = {
+    clock: clock,
+    griddler: griddler
+  };
+
+  var planFns = _.pluck(guyCells, 'plan');
+  var q = queue();
+  planFns.forEach(function queuePlan(planFn) {
+    q.defer(planFn, planPack);
+  });
+  q.awaitAll(execute);
+}
+
+function execute(error) {
+  if (error) {
+    console.log(error);
+  }
+
+  clock.tick();
+}
+
+function advanceAllToNextState() {
+  updateCells();
+  renderLayers();
+}
+
+function updateCells() {
+  floorCells.forEach(updateCell);
+  guyCells.forEach(updateCell);
+}
+
+function updateCell(cell) {
+  // TODO: _.cloneDeep?
+  if (cell.next) {
+    cell.live = cell.next;
+    cell.next = {};
+  }
+}
+
+function renderLayers() {
+  floorRenderer.render(griddler.getLayer('floor'));
+  guyRenderer.render(griddler.getLayer('guys'));  
+}
+
+function generateFloorCell() {
+  return {
+    id: idmaker.randomId(5),
+    live: {
+      coords: [
+        probable.roll(10),
+        probable.roll(10)
+      ]
+    }
+  };
+}
+
+function generateGuyCell() {
+  var guy = {
+    id: idmaker.randomId(5),
+    plan: function plan(opts, done) {
+      opts.clock.queueAction(createMove({
+        actor: guy,
+        griddler: opts.griddler,
+        vector: pickRandomVector()
+      }));
+      callBackOnNextTick(done);
+    },
+    live: {
+      coords: [
+        probable.roll(10),
+        probable.roll(10)
+      ]
+    },
+    next: {
+      coords: []
+    }
+  };
+
+  return guy;
+}
+
+var range = [-1, 0, 1];
+
+function pickRandomVector() {
+  return [probable.pickFromArray(range), probable.pickFromArray(range)];
 }
 
 module.exports = {
